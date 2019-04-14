7701bb9ee4c0dc8c878ecdba2be297f819059bf2
Jesús Calleja Rodríguez <jesuscallejatf97@gmail.com>
2019-04-14 21:52:54 +0200

    Rename Activity

--- app/src/main/java/com/calleja/jesus/moneymanager/activities/ForgotPassActivity.kt
+++ app/src/main/java/com/calleja/jesus/moneymanager/activities/ForgotPassActivity.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.calleja.jesus.moneymanager.R
 
-class ForgetPassActivity : AppCompatActivity() {
+class ForgotPassActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
