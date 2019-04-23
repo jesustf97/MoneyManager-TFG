22d3ff13d2290b383b679f14ff5dd9e27e82248c
Jesús Calleja Rodríguez <jesuscallejatf97@gmail.com>
2019-04-23 18:33:50 +0200

    Implementar Info usuario y Log out

--- app/src/main/java/com/calleja/jesus/moneymanager/fragments/InfoFragment.kt
+++ app/src/main/java/com/calleja/jesus/moneymanager/fragments/InfoFragment.kt
@@ -7,14 +7,57 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.calleja.jesus.moneymanager.R
+import com.calleja.jesus.moneymanager.utils.CircleTransform
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_info.view.*
+
 
 class InfoFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+    private lateinit var _view: View
+
+    private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var currentUser: FirebaseUser
+    //private val store: FirebaseFirestore = FirebaseFirestore.getInstance()
+    //private lateinit var chatDBRef: CollectionReference
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        _view =  inflater.inflate(R.layout.fragment_info, container, false)
+
+        //setUpChatDB()
+        setUpCurrentUser()
+        setUpCurrentUserInfoUI()
+
+        return _view
     }
 
+  /*  private fun setUpChatDB() {
+        chatDBRef = store.collection("chat")
+    }*/
+
+    private fun setUpCurrentUser() {
+        currentUser = mAuth.currentUser!!
+    }
+
+    private fun setUpCurrentUserInfoUI() {
+        _view.textViewInfoEmail.text = currentUser.email
+        _view.textViewInfoName.text = currentUser.displayName?.let {currentUser.displayName}
+            ?: run {getString(R.string.info_no_name)}
+
+        currentUser.photoUrl?.let {
+            Picasso.get().load(currentUser.photoUrl).resize(300,300)
+                .centerCrop().transform(CircleTransform()).into(_view.imageViewInfoAvatar)
+        } ?: run {
+            Picasso.get().load(R.drawable.ic_person).resize(300,300)
+                .centerCrop().transform(CircleTransform()).into(_view.imageViewInfoAvatar)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
 
 }
